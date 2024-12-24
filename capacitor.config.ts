eb74ad25eac1b25c5df650deccbafc56768234e5
kayloko <cherif.aymen@esprit.tn>
2024-12-24 23:56:13 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cineman',
+  webDir: 'dist'
+};
+
+export default config;
